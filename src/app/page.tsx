7ef64019248a22ae4d67315e4062edb1a98506fd
gabihakcba <gabrielhakcba@gmail.com>
2024-03-12 19:14:06 -0300

    Middleware para proteccion de rutas y loggeo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ const signIn = async (router, data, setLogginFailed) => {
   }
   const response = await signInAccount(user)
 
-  if(response.status === 200) {
+  if (response.status === 200) {
     router.push('/admin')
   }
   else {
@@ -37,41 +37,71 @@ export default function Home() {
     username: '',
     password: ''
   })
-  useEffect(() => {
-    const cookie = parse(`${document.cookie}` || '')
-    if(cookie.auth) {
-      router.push('/admin')
-    }
-  }, [router])
+  
+  /**
+   * Esta comprobacion esta en el middleware
+   */
+  // useEffect(() => {
+  //   const cookie = parse(`${document.cookie}` || '')
+  //   if (cookie.auth) {
+  //     router.push('/admin')
+  //   }
+  // }, [router])
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 h-max">
         <div className="mb-4">
-          <label className="block text-gray-700 text-base font-bold mb-2" htmlFor="username">
+          <label
+            className="block text-gray-700 text-base font-bold mb-2"
+            htmlFor="username">
             Username
           </label>
-          <input name="password" onChange={(e) => handleChange(e, setData)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input>
+          <input
+            name="password"
+            onChange={(e) => handleChange(e, setData)}
+            className="inputForm"
+            id="username"
+            type="text"
+            placeholder="Username"
+            autoComplete="off">
+          </input>
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 text-base font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-base font-bold mb-2"
+            htmlFor="password">
             Password
           </label>
-          <input name="password" onChange={(e) => handleChange(e, setData)} className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
+          <input
+            name="password"
+            onChange={(e) => handleChange(e, setData)}
+            className="inputForm"
+            id="password"
+            type="password"
+            placeholder="******************">
+          </input>
         </div>
         <div className="flex flex-col">
           <div className="flex items-center justify-between flex-col sm:flex-row">
-            <button onClick={() => signIn(router, data, setLogginFailed)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            <button
+              onClick={() => signIn(router, data, setLogginFailed)}
+              className="blueButtonForm"
+              type="button">
               Sign In
             </button>
-            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            <a
+              className="linkBlueForm"
+              href="#">
               Forgot Password?
             </a>
           </div>
           {
             logginFailed &&
             <div className="flex justify-center">
-              <p className="text-red-500 text-sm italic">Wrong password or user name. Try again</p>
+              <p className="text-red-500 text-sm italic">
+                Wrong password or user name. Try again
+              </p>
             </div>
           }
         </div>
---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies"
+import { NextRequest, NextResponse } from "next/server"
+import { jwtVerify } from "jose"
+import { cookies } from 'next/headers'
+
+const verifyCookie = async (cookie: RequestCookie): Promise<boolean> => {
+  if (typeof cookie === 'undefined') return false
+  try {
+    const parsed: string = cookie.value
+    const payload = await jwtVerify(parsed, new TextEncoder().encode('secret'))
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+export async function middleware(req: NextRequest): Promise<NextResponse> {
+  const adminPathReg: RegExp = /^\/admin/i
+  const loginPath: string = '/'
+  const pathname: string = req.nextUrl.pathname
+  
+  if (pathname.startsWith('/_next') || pathname.startsWith('/fav') || pathname.startsWith('/api/login')) {
+    return NextResponse.next();
+  }
+  
+  const cookie: RequestCookie = req.cookies.get('auth')
+  const verifiedCookie = await verifyCookie(cookie)
+  
+  if (!verifiedCookie && pathname !== loginPath) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+  else if (verifiedCookie && pathname === loginPath) {
+    return NextResponse.redirect(new URL('/admin', req.url))
+  }
+  else if (verifiedCookie && pathname !== loginPath) {
+    return NextResponse.next()
+  }
+
+  return NextResponse.next()
+}
