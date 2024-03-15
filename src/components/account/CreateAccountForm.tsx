7945969cb7be208aff27843a95e84210b0a83bfc
gabihakcba <gabrielhakcba@gmail.com>
2024-03-15 01:30:29 -0300

    Formulario para crear cuenta

---
+++ src/components/account/CreateAccountForm.tsx
@@ -0,0 +1,179 @@
+'use client'
+
+import { type ReactElement } from 'react'
+import { Permissions, type Setter } from 'utils/types'
+import { useForm } from 'react-hook-form'
+
+export function CreateAccountForm({
+  setIsOpen,
+  setAccounts,
+  setPages,
+  sendForm
+}: any): ReactElement {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch
+  } = useForm()
+
+  return (
+    <form
+      onSubmit={handleSubmit((data) => {
+        void sendForm(
+          data,
+          setIsOpen,
+          setAccounts as Setter,
+          setPages as Setter
+        )
+      })}
+      className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 h-max w-max flex flex-col gap-0'
+      id='createForm'
+    >
+      <div className='mb-4'>
+        <label
+          className='block text-gray-700 text-base font-bold mb-2'
+          htmlFor='username'
+        >
+          Nombre de Usuario
+        </label>
+        <input
+          {...register('username', {
+            required: {
+              value: true,
+              message: 'El nombre de usuario es requerido'
+            }
+          })}
+          name='username'
+          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+          id='username'
+          form='createForm'
+          type='text'
+          autoComplete='off'
+          placeholder='Nombre de usuario'
+        ></input>
+        {errors?.username && (
+          <span className='inputError'>
+            {errors.username.message as string}
+          </span>
+        )}
+      </div>
+      <div className='mb-4'>
+        <label
+          className='block text-gray-700 text-base font-bold mb-2'
+          htmlFor='password'
+        >
+          Contraseña
+        </label>
+        <input
+          {...register('password', {
+            required: {
+              value: true,
+              message: 'La contraseña es requerida'
+            }
+          })}
+          name='password'
+          id='password'
+          form='createForm'
+          type='password'
+          className='shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          placeholder='******************'
+        ></input>
+        {errors?.password && (
+          <span className='inputError'>
+            {errors.password.message as string}
+          </span>
+        )}
+      </div>
+      <div className='mb-4'>
+        <label
+          className='block text-gray-700 text-base font-bold mb-2'
+          htmlFor='repeatpassword'
+        >
+          Repetir Contraseña
+        </label>
+        <input
+          {...register('repeatpassword', {
+            required: {
+              value: true,
+              message: 'Confirmar la contraseña es requerido'
+            },
+            validate: (value) => {
+              return (
+                watch('password') === value || 'Las contraseñas deben coincidir'
+              )
+            }
+          })}
+          name='repeatpassword'
+          id='repeatpassword'
+          form='createForm'
+          type='password'
+          className='shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+          placeholder='******************'
+        ></input>
+        {errors?.repeatpassword && (
+          <span className='inputError'>
+            {errors.repeatpassword.message as string}
+          </span>
+        )}
+      </div>
+      <div className='mb-4'>
+        <label
+          className='block text-gray-700 text-base font-bold mb-2'
+          htmlFor='permisos'
+        >
+          Permisos
+        </label>
+        <select
+          {...register('permissions', {
+            required: {
+              value: true,
+              message: 'Los permisos son requeridos'
+            },
+            validate: (value) => {
+              return (
+                value !== 'OTHER' || 'Debe seleccionar los permisos adecuados'
+              )
+            }
+          })}
+          name='permissions'
+          id='permissions'
+          form='createForm'
+          defaultValue={Permissions.OTHER}
+          className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+        >
+          <option value={Permissions.OWN}>Propietario</option>
+          <option value={Permissions.ADM}>Administrador</option>
+          <option value={Permissions.INS}>Instructor</option>
+          <option value={Permissions.MEM}>Alumno</option>
+          <option value={Permissions.OTHER}>Otro</option>
+        </select>
+        {errors?.permissions && (
+          <span className='inputError'>
+            {errors.permissions.message as string}
+          </span>
+        )}
+      </div>
+      <div className='flex flex-col'>
+        <div className='flex items-stretch justify-start gap-2 flex-col md:flex-row md:items-center md:gap-10'>
+          <button
+            form='createForm'
+            className='mb-2 md:mb-auto py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline'
+            type='submit'
+          >
+            Crear
+          </button>
+          <button
+            onClick={() => {
+              setIsOpen((prev: boolean) => !prev)
+            }}
+            className='mb-2 md:mb-auto py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded focus:outline-none focus:shadow-outline'
+            type='button'
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </form>
+  )
+}
