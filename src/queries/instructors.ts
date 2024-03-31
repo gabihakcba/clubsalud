b7b32b2f8675c524c3585e91e596dd1d6924e0b3
gabihakcba <gabrielhakcba@gmail.com>
2024-03-31 05:02:26 -0300

    assign class and instructor

--- src/queries/instructors.ts
+++ src/queries/instructors.ts
@@ -1,26 +1,44 @@
 import { type Instructor, type CreateInstructor } from 'utils/types'
 import axios from 'axios'
 
-interface GetInsResponse {
-  _: Response
+interface GetInsInstructor {
+  _: Response | null
   data: Instructor
 }
 
 export const getInstructorById = async (
-  id: number
-): Promise<GetInsResponse> => {
+  id: number | undefined
+): Promise<GetInsInstructor> => {
+  if (!id) {
+    return {
+      _: null,
+      data: {
+        id: 0,
+        name: '',
+        lastName: '',
+        dni: BigInt(0),
+        phoneNumber: BigInt(0),
+        address: '',
+        email: '',
+        degree: '',
+        accountId: 0
+      }
+    }
+  }
   return await axios.get(`http://localhost:3000/api/instructors/${id}`)
 }
 
 export const createInstructor = async (
   newInstructor: CreateInstructor
-): Promise<GetInsResponse> => {
+): Promise<GetInsInstructor> => {
   return await axios.post('http://localhost:3000/api/instructors', {
     ...newInstructor
   })
 }
 
-export const deleteInstructor = async (id: number): Promise<GetInsResponse> => {
+export const deleteInstructor = async (
+  id: number
+): Promise<GetInsInstructor> => {
   return await axios.delete('http://localhost:3000/api/instructors', {
     data: {
       id
@@ -30,8 +48,14 @@ export const deleteInstructor = async (id: number): Promise<GetInsResponse> => {
 
 export const updateInstructor = async (
   instructor: Instructor
-): Promise<GetInsResponse> => {
+): Promise<GetInsInstructor> => {
   return await axios.patch('http://localhost:3000/api/instructors', {
     ...instructor
   })
+}
+
+export async function getInstructorByName(
+  name: string
+): Promise<GetInsInstructor> {
+  return await axios.get(`http://localhost:3000/api/instructors/${name}`)
 }
