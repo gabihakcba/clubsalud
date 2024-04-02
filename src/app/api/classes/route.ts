d7c4dce4be3a91d6ec34fc0e97f6645f4b2fece5
gabihakcba <gabrielhakcba@gmail.com>
2024-04-02 20:02:55 -0300

    classes crud

--- src/app/api/classes/route.ts
+++ src/app/api/classes/route.ts
@@ -2,7 +2,7 @@ import { type Class, type PrismaClient } from '@prisma/client'
 import prisma from 'utils/prisma'
 import JSONbig from 'json-bigint'
 import { type NextRequest } from 'next/server'
-import { type CreateClass } from 'utils/types'
+import { Class_, CreateClass_ } from 'utils/types'
 
 const db: PrismaClient = prisma
 
@@ -21,14 +21,62 @@ export async function GET(): Promise<Response> {
 
 export async function POST(req: NextRequest): Promise<Response> {
   try {
-    const data: CreateClass = await req.json()
-    const class_: Class = await db.class.create({ data })
+    const data: CreateClass_ = await req.json()
+    const parsed = {
+      name: data.name,
+      duration: parseFloat(String(data.duration))
+    }
+    const class_: Class = await db.class.create({ data: parsed })
     return new Response(JSONbig.stringify(class_), {
       status: 200
     })
   } catch (error) {
     return new Response(JSONbig.stringify(error), {
       status: 500
+    })
+  }
+}
+
+export async function PATCH(req: NextRequest): Promise<Response> {
+  try {
+    const data: Class_ = await req.json()
+    const id: number = Number(data.id)
+    const parsed: Class = {
+      id,
+      name: data.name,
+      duration: parseFloat(String(data.duration))
+    }
+    const res: Class_ = await db.class.update({
+      where: {
+        id
+      },
+      data: parsed
+    })
+    return new Response(JSONbig.stringify(res), {
+      status: 200
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response('No se pudo editar la clase', {
+      status: 400
+    })
+  }
+}
+
+export async function DELETE(req: NextRequest): Promise<Response> {
+  try {
+    const data: { id: number } = await req.json()
+    const res: Class_ = await db.class.delete({
+      where: {
+        id: data.id
+      }
+    })
+    return new Response(JSONbig.stringify(res), {
+      status: 200
+    })
+  } catch (error) {
+    return new Response('No se pudo eliminar la clase', {
+      status: 400
     })
   }
 }
--- src/components/classes/ClassCard.tsx
+++ src/components/classes/ClassCard.tsx
@@ -1,115 +1,83 @@
-import { useState, type ReactElement } from 'react'
-import { useForm } from 'react-hook-form'
+import { ReactElement } from 'react'
+import { Class_ } from 'utils/types'
 import Image from 'next/image'
+import gym from '../../../public/images/gym_image.png'
 import edit from '../../../public/edit.svg'
-import { type Class } from 'utils/types'
+import delete_ from '../../../public/delete_.svg'
+import Modal from 'components/Modal'
+import { useModal } from 'utils/useModal'
+import ClassForm from './ClassForm'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteClass } from 'queries/classes'
 
-export default function ClassCard({ class_ }: { class_: Class }): ReactElement {
-  const [editF, setEditF] = useState(false)
-  const {
-    register,
-    // handleSubmit,
-    formState: { errors }
-    // reset,
-    // watch
-  } = useForm()
+export default function ClassCard2({
+  class_
+}: {
+  class_: Class_
+}): ReactElement {
+  const query = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: deleteClass,
+    async onSuccess(variables) {
+      await query.setQueryData(['class'], (oldData: Class_[]) => {
+        const index = oldData.findIndex((class_: Class_) => {
+          return class_.id === variables.data.id
+        })
+        const newData = [...oldData]
+        newData.splice(index, 1)
+        return newData
+      })
+    }
+  })
+
+  const [isOpen, openModal, closeModal] = useModal()
+
   return (
-    <>
-      <form
-        action=''
-        id={`classes${class_?.id}`}
-        // onSubmit={handleSubmit((data) => {
-        // mutateC({ id: instructor.id, data })
-        // })}
-      >
-        <div className='flex items-center justify-end pb-2'>
-          <button
-            onClick={() => {
-              setEditF((prev: boolean) => !prev)
-            }}
-            type='button'
-            className='text-sm font-medium text-blue-600 hover:underline dark:text-blue-500'
-          >
-            <Image
-              src={edit}
-              width={30}
-              height={30}
-              alt='E'
-            ></Image>
-          </button>
-        </div>
-        <div className='flex flex-row'>
-          <ul
-            role='list'
-            className='divide-y divide-gray-200 dark:divide-gray-700 w-full'
-          >
-            <li className='flex flex-row items-center justify-between w-full mb-1 mt-1'>
-              <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>
-                Nombre
-              </label>
-              {!editF && (
-                <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>
-                  {class_?.name}
-                </label>
-              )}
-              {editF && (
-                <div>
-                  <input
-                    type='text'
-                    id='name'
-                    form={`class${class_?.id}`}
-                    className='w-36 bg-gray-50 border text-right border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    defaultValue={class_?.name}
-                    {...register('name', {
-                      required: {
-                        value: true,
-                        message: 'El nombre requerido'
-                      }
-                    })}
-                  />
-                  {errors?.name && (
-                    <span className='inputError'>
-                      {errors.name.message as string}
-                    </span>
-                  )}
-                </div>
-              )}
-            </li>
-            <li className='flex flex-row items-center justify-between w-full mb-1 mt-1'>
-              <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>
-                Duración (hs)
-              </label>
-              {!editF && (
-                <label className='block mb-2 text-lg font-medium text-gray-900 dark:text-white'>
-                  {class_?.duration}
-                </label>
-              )}
-              {editF && (
-                <div>
-                  <input
-                    type='number'
-                    id='duration'
-                    form={`class${class_?.id}`}
-                    className='w-36 bg-gray-50 border text-right border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    defaultValue={class_?.duration}
-                    {...register('duration', {
-                      required: {
-                        value: true,
-                        message: 'La duración es requerida'
-                      }
-                    })}
-                  />
-                  {errors?.duration && (
-                    <span className='inputError'>
-                      {errors.duration.message as string}
-                    </span>
-                  )}
-                </div>
-              )}
-            </li>
-          </ul>
-        </div>
-      </form>
-    </>
+    <div className='w-max flex flex-col'>
+      <div className='self-end'>
+        <button onClick={openModal}>
+          <Image
+            src={edit}
+            width={20}
+            height={20}
+            alt='E'
+          ></Image>
+        </button>
+        <Modal
+          isOpen={isOpen}
+          closeModal={closeModal}
+        >
+          <ClassForm
+            class_={class_}
+            closeModal={closeModal}
+          ></ClassForm>
+        </Modal>
+        <button
+          onClick={() => {
+            mutate(class_.id)
+          }}
+        >
+          <Image
+            src={delete_}
+            width={20}
+            height={20}
+            alt='E'
+          ></Image>
+        </button>
+      </div>
+      <div className=''>
+        <Image
+          src={gym}
+          width={200}
+          height={100}
+          alt='gym-image'
+        ></Image>
+      </div>
+      <div className='mt-4 flex justify-between items-center'>
+        <span className='text-xl font-semibold'>{class_.name}</span>
+        <span>{class_.duration} hora/s</span>
+      </div>
+    </div>
   )
 }
