5de689310e59d5fc76467391230d2c0d1898406f
gabihakcba <gabrielhakcba@gmail.com>
2024-04-07 18:52:59 -0300

    hotfix prod path end with /

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,5 +1,5 @@
 export const dev = 'http://localhost:3000'
-export const production = 'https://clubsalud-gabihakcbas-projects.vercel.app/'
+export const production = 'https://clubsalud-gabihakcbas-projects.vercel.app'
 
 export const path = (): string => {
   if (process.env.NODE_ENV === 'production') {
