db9f58e9b484f4cd09af733cda81f2d3561d00b2
gabihakcba <gabrielhakcba@gmail.com>
2024-04-08 03:37:45 -0300

    testing new prisma service

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,13 +1,32 @@
+// import { PrismaClient } from '@prisma/client'
+
+// let prisma: PrismaClient = new PrismaClient()
+
+// if (process.env.NODE_ENV === 'production') {
+//   // In production, use a singleton instance to avoid creating multiple connections
+//   prisma = prisma || new PrismaClient()
+// } else {
+//   // In development, create a new instance for each request
+//   prisma = new PrismaClient()
+// }
+
+// export default
+
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient = new PrismaClient()
+declare global {
+  var prisma: PrismaClient
+}
+
+let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  // In production, use a singleton instance to avoid creating multiple connections
-  prisma = prisma || new PrismaClient()
+  prisma = global.prisma || new PrismaClient()
 } else {
-  // In development, create a new instance for each request
-  prisma = new PrismaClient()
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
 }
 
 export default prisma
