061d58bd9ce6d53097446063d42740c30cbb7cf3
gabihakcba <gabrielhakcba@gmail.com>
2024-04-09 14:52:34 -0300

    Set path to new branch development

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,10 +1,12 @@
 export const dev = 'http://localhost:3000'
-export const production = 'https://clubsalud-gabihakcbas-projects.vercel.app'
-export const ownDomain = 'https://www.centromedicomedintt.com/'
+export const development = 'https://clubsalud.vercel.app/'
+export const production = 'https://www.centromedicomedintt.com/'
 
 export const path = (): string => {
   if (process.env.NODE_ENV === 'production') {
-    return ownDomain
+    return production
+  } else if (process.env.NODE_ENV === 'development') {
+    return development
   } else {
     return dev
   }
