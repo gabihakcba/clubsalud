511dcb3042cbbce6996267fae8d3aa2282767a49
gabihakcba <gabrielhakcba@gmail.com>
2024-04-10 20:03:43 -0300

    rename enviroment variables

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,10 +3,10 @@ export const test = 'https://clubsalud.vercel.app'
 export const production = 'https://www.centromedicomedintt.com'
 
 export const path = (): string => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
     console.log('production')
     return production
-  } else if (process.env.NODE_ENV === 'development') {
+  } else if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
     console.log('development')
     return development
   } else {
