237bbd81d0102bf61a89aeeb8b84d8c188988256
gabihakcba <gabrielhakcba@gmail.com>
2024-04-11 00:25:43 -0300

    health plan create and get

---
+++ src/queries/health.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { path } from 'utils/path'
+import { type CreateHealthPlan, type HealthPlan } from 'utils/types'
+
+export const getHealthPlans = async (): Promise<HealthPlan[]> => {
+  const response = await axios.get(`${path()}/api/health`)
+  return response.data
+}
+
+export const createHealthPlan = async (
+  data: CreateHealthPlan
+): Promise<HealthPlan> => {
+  const response = await axios.post(`${path()}/api/health`, data)
+  return response.data
+}
