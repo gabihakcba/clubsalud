dca505e93abf92044650d95c79245fbb871df1a6
gabihakcba <gabrielhakcba@gmail.com>
2024-04-12 21:11:20 -0300

    refact permissions

--- prisma/migrations/20240406024540_add_total_required_to_subscription/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `total` on table `Subscription` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Subscription" ALTER COLUMN "total" SET NOT NULL;
