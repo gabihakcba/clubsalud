e30a5fae375b2a610e3f9be25413dbb5133fdc6f
gabihakcba <gabrielhakcba@gmail.com>
2024-04-13 21:12:51 -0300

    list members health subscriptions

--- src/app/api/healthSubscribed/route.ts
+++ src/app/api/healthSubscribed/route.ts
@@ -21,3 +21,22 @@ export async function POST(req: NextRequest): Promise<Response> {
     })
   }
 }
+
+export async function DELETE(req: NextRequest): Promise<Response> {
+  try {
+    const data: { id: number } = await req.json()
+    const res: HealthPlanSubscribed = await db.healthPlanSubscribed.delete({
+      where: {
+        id: Number(data.id)
+      }
+    })
+    return new Response(JSON.stringify(res), {
+      status: 200
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response('No se pudo eliminar la obra social', {
+      status: 400
+    })
+  }
+}
