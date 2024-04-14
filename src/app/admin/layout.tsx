153b3a5c960f443304e28480f597dc6906ac460e
gabihakcba <gabrielhakcba@gmail.com>
2024-04-14 17:14:42 -0300

    create, get and show notifications

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -102,8 +102,8 @@ export default function AdminLayout({ children }: any): ReactElement {
                 </li>
                 <li>
                   <Link
-                    href='#'
-                    className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname === '/todo' ? 'bg-gray-500' : ''}`}
+                    href='/admin/notifications'
+                    className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname === '/admin/notifications' ? 'bg-gray-500' : ''}`}
                   >
                     <svg
                       className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
