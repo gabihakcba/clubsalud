ec358d60939873a8964fd4ef4176b5a42d7b156d
gabihakcba <gabrielhakcba@gmail.com>
2024-04-15 01:15:52 -0300

    websocket dinamic paths

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -1,4 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3001')
+const socket = io(
+  process.env.NEXT_PUBLIC_WS_SERVER_URL ?? 'http://localhost:3001'
+)
 export default socket
