c7d92f2c377cf0655f8a379bfabf322845315df3
gabihakcba <gabrielhakcba@gmail.com>
2024-04-16 21:25:35 -0300

    just account owned notifications && websocet paused until research of free websocet server on vercel (see SWR)

--- src/components/notifications/NotificationSection.tsx
+++ src/components/notifications/NotificationSection.tsx
@@ -9,23 +9,25 @@ export default function NotificationSection({
   notifications
 }: params): ReactElement {
   return (
-    <section
-      className='mt-5 ml-5 h-full'
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(12rem,1fr))',
-        gap: '1rem',
-        alignContent: 'flex-start'
-      }}
-    >
-      {notifications?.map((notification, index) => (
-        <NotificationCard
-          notification={notification}
-          key={index}
-        ></NotificationCard>
-      ))}
-    </section>
+    <div>
+      <section
+        className='mt-5 ml-5 h-full'
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(12rem,1fr))',
+          gap: '1rem',
+          alignContent: 'flex-start'
+        }}
+      >
+        {notifications?.map((notification, index) => (
+          <NotificationCard
+            notification={notification}
+            key={index}
+          ></NotificationCard>
+        ))}
+      </section>
+    </div>
   )
 }
