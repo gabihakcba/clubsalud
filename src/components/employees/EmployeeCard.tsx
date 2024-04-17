9f23730dd9bd2fa09bf657f0795402119ca4acc7
gabihakcba <gabrielhakcba@gmail.com>
2024-04-17 18:46:54 -0300

    delete and update employees

--- src/components/employees/EmployeeCard.tsx
+++ src/components/employees/EmployeeCard.tsx
@@ -1,13 +1,17 @@
 import HasRole from 'components/HasRole'
+import Modal from 'components/Modal'
 import { type ReactElement } from 'react'
 import { Permissions, type Employee } from 'utils/types'
+import { useModal } from 'utils/useModal'
+import UpdateEmployeeForm from './UpdateEmployeeForm'
 
 interface params {
   employee: Employee
 }
 export default function EmployeeCard({ employee }: params): ReactElement {
+  const [edit, openEdit, closeEdit] = useModal(false)
   return (
-    <div className='flex flex-col gap-2 border rounded p-2'>
+    <div className='flex flex-col gap-2 border rounded p-2 justify-between'>
       <h5 className='font-bold'>
         {employee.name} {employee.lastName}
       </h5>
@@ -16,10 +20,23 @@ export default function EmployeeCard({ employee }: params): ReactElement {
         {employee.alias && <p>Alias: {employee.alias}</p>}
         {employee.cbu !== undefined && <p>CBU: {String(employee.cbu)}</p>}
       </HasRole>
-      <div className='flex gap-2'>
-        <button className='light-blue-border-button text-sm'>Editar</button>
-        <button className='light-red-border-button text-sm'>Eliminar</button>
-      </div>
+      <HasRole required={[Permissions.OWN]}>
+        <div className='flex gap-2'>
+          <button
+            className='light-blue-border-button text-sm'
+            onClick={openEdit}
+          >
+            Info
+          </button>
+          <Modal
+            isOpen={edit}
+            closeModal={closeEdit}
+          >
+            <UpdateEmployeeForm employee={employee}></UpdateEmployeeForm>
+          </Modal>
+          {/* <button className='light-red-border-button text-sm'>Eliminar</button> */}
+        </div>
+      </HasRole>
     </div>
   )
 }
