521f202fdfc21cc510c1cf6a2cb80c8216f3b295
gabihakcba <gabrielhakcba@gmail.com>
2024-04-18 01:44:40 -0300

    CRUD employee payment

---
+++ src/app/admin/payments/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { EmplooyeePaymentsSection } from 'components/payments/EmployeePaymentsSection'
+import { getEmployeePayments } from 'queries/employeePayments'
+import { type ReactElement } from 'react'
+
+export default function Page(): ReactElement {
+  const { data: employeePayments } = useQuery({
+    queryKey: ['employeePayments'],
+    queryFn: async () => {
+      return await getEmployeePayments()
+    }
+  })
+  return (
+    <div className='w-full h-full flex flex-col'>
+      <EmplooyeePaymentsSection
+        employeePayments={employeePayments}
+      ></EmplooyeePaymentsSection>
+    </div>
+  )
+}
