0bc8dcb91a96aabb064d706e72c95a0d81766e1b
gabihakcba <gabrielhakcba@gmail.com>
2024-05-02 19:15:12 -0300

    CRUD classes & CRUD offers

--- src/components/classes/CreateClassForm.tsx
+++ src/components/classes/CreateClassForm.tsx
@@ -3,19 +3,17 @@ import { useForm } from 'react-hook-form'
 import { type Class_, type CreateClass_ } from 'utils/types'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createClass } from 'queries/classes'
+import { InputText } from 'primereact/inputtext'
+import { Button } from 'primereact/button'
 
-interface params {
-  closeModal: () => void
-}
-export default function CreateClassForm({ closeModal }: params): ReactElement {
+export default function CreateClassForm(): ReactElement {
   const query = useQueryClient()
 
-  const { mutate } = useMutation({
+  const { mutate: create, isPending } = useMutation({
     mutationFn: createClass,
     async onSuccess(data) {
       reset()
-      closeModal()
-      await query.setQueryData(['class'], (oldData: Class_[]) => [
+      await query.setQueryData(['classes'], (oldData: Class_[]) => [
         ...oldData,
         data.data
       ])
@@ -30,76 +28,54 @@ export default function CreateClassForm({ closeModal }: params): ReactElement {
   } = useForm()
 
   return (
-    <div className='w-full h-full bg-white rounded p-2'>
+    <div className='w-full h-full'>
       <form
         action=''
         id={'class'}
-        className='flex flex-col justify-start items-end'
+        className='flex flex-column justify-content-start align-items-end gap-4 pt-4'
         onSubmit={handleSubmit((data) => {
-          mutate(data as CreateClass_)
+          create(data as CreateClass_)
         })}
       >
-        <div className='flex flex-row'>
-          <ul
-            role='list'
-            className='divide-y divide-gray-200 dark:divide-gray-700 w-full'
-          >
-            <li className='flex flex-row items-center justify-between w-full mb-1 mt-1'>
-              <label className='block mb-2 text-lg font-medium text-gray-900'>
-                Nombre
-              </label>
-              <div>
-                <input
-                  type='text'
-                  id='name'
-                  form={'class'}
-                  className='w-36 bg-gray-50 border text-right border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  {...register('name', {
-                    required: {
-                      value: true,
-                      message: 'El nombre requerido'
-                    }
-                  })}
-                />
-                {errors?.name && (
-                  <span className='inputError'>
-                    {errors.name.message as string}
-                  </span>
-                )}
-              </div>
-            </li>
-            <li className='flex flex-row items-center justify-between w-full mb-1 mt-1'>
-              <label className='block mb-2 text-lg font-medium text-gray-900'>
-                Duración (hs)
-              </label>
-              <div>
-                <input
-                  type='number'
-                  id='duration'
-                  form={'class'}
-                  className='w-36 bg-gray-50 border text-right border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                  {...register('duration', {
-                    required: {
-                      value: true,
-                      message: 'La duración es requerida'
-                    }
-                  })}
-                />
-                {errors?.duration && (
-                  <span className='inputError'>
-                    {errors.duration.message as string}
-                  </span>
-                )}
-              </div>
-            </li>
-          </ul>
-        </div>
-        <button
-          className='blueButtonForm p-1'
+        <li className='p-float-label'>
+          <InputText
+            type='text'
+            id='name'
+            form={'class'}
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'El nombre requerido'
+              }
+            })}
+            invalid={errors?.name !== undefined}
+          />
+          <label>Nombre</label>
+        </li>
+        <li className='p-float-label'>
+          <InputText
+            type='number'
+            id='duration'
+            form={'class'}
+            {...register('duration', {
+              required: {
+                value: true,
+                message: 'La duración es requerida'
+              }
+            })}
+            invalid={errors?.duration !== undefined}
+          />
+          <label>Duración (hs)</label>
+        </li>
+        <Button
           type='submit'
-        >
-          Enviar
-        </button>
+          label='Enviar'
+          size='small'
+          className='w-full'
+          icon='pi pi-upload'
+          iconPos='right'
+          loading={isPending}
+        />
       </form>
     </div>
   )
