25412faa6c9c136773bfd0cd4c02bf84a8198f86
gabihakcba <gabrielhakcba@gmail.com>
2024-05-02 19:15:12 -0300

    CRUD classes & CRUD offers

--- src/components/classes/ClassCard.tsx
+++ src/components/classes/ClassCard.tsx
@@ -2,14 +2,13 @@ import { type ReactElement } from 'react'
 import { type Class_, Permissions } from 'utils/types'
 import Image from 'next/image'
 import gym from '../../../public/images/gym_image.png'
-import edit from '../../../public/edit.svg'
-import delete_ from '../../../public/delete_.svg'
 import Modal from 'components/Modal'
 import { useModal } from 'utils/useModal'
 import ClassForm from './ClassForm'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { deleteClass } from 'queries/classes'
 import HasRole from 'components/HasRole'
+import { Button } from 'primereact/button'
 
 export default function ClassCard2({
   class_
@@ -35,50 +34,39 @@ export default function ClassCard2({
   const [isOpen, openModal, closeModal] = useModal()
 
   return (
-    <div className='w-max flex flex-col'>
+    <div className='w-max flex flex-column'>
       <HasRole required={[Permissions.ADM, Permissions.OWN]}>
-        <div className='self-end'>
-          <button onClick={openModal}>
-            <Image
-              src={edit}
-              width={20}
-              height={20}
-              alt='E'
-            ></Image>
-          </button>
+        <div className='align-self-end'>
+          <Button
+            onClick={openModal}
+            icon='pi pi-pen-to-square'
+            size='small'
+            outlined
+          />
           <Modal
             isOpen={isOpen}
             closeModal={closeModal}
           >
-            <ClassForm
-              class_={class_}
-              closeModal={closeModal}
-            ></ClassForm>
+            <ClassForm class_={class_}></ClassForm>
           </Modal>
-          <button
+          <Button
             onClick={() => {
               mutate(class_.id)
             }}
-          >
-            <Image
-              src={delete_}
-              width={20}
-              height={20}
-              alt='E'
-            ></Image>
-          </button>
+            icon='pi pi-trash'
+            size='small'
+            outlined
+            severity='danger'
+          />
         </div>
       </HasRole>
-      <div className=''>
-        <Image
-          src={gym}
-          width={200}
-          height={100}
-          alt='gym-image'
-        ></Image>
-      </div>
-      <div className='mt-4 flex justify-between items-center'>
-        <span className='text-xl font-semibold'>{class_.name}</span>
+      <Image
+        src={gym}
+        width={200}
+        alt='gym-image'
+      ></Image>
+      <div className='mt-4 flex justify-content-between align-items-center'>
+        <span className='font-semibold'>{class_.name}</span>
         <span>{class_.duration} hora/s</span>
       </div>
     </div>
