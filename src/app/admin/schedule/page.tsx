a3a6ef3f35dfa7ca0d32886cbdabccbdd3cc2808
gabihakcba <gabrielhakcba@gmail.com>
2024-06-04 04:29:31 -0300

    hot fix

--- src/app/admin/schedule/page.tsx
+++ src/app/admin/schedule/page.tsx
@@ -41,7 +41,9 @@ const formatScheduler = (schedules: Schedule[]): any[] => {
     { start: 1330, end: 1400, classes: [] },
     { start: 1400, end: 1430, classes: [] },
     { start: 1430, end: 1500, classes: [] },
-    { start: 1500, end: 1630, classes: [] },
+    { start: 1500, end: 1530, classes: [] },
+    { start: 1530, end: 1600, classes: [] },
+    { start: 1600, end: 1630, classes: [] },
     { start: 1630, end: 1700, classes: [] },
     { start: 1700, end: 1730, classes: [] },
     { start: 1730, end: 1800, classes: [] }
