2ffa65925959db218d89f0b9e8f910c47a51350e
gabihakcba <gabrielhakcba@gmail.com>
2024-06-06 01:57:40 -0300

    accounting payments and bills

--- src/app/admin/accounting/page.tsx
+++ src/app/admin/accounting/page.tsx
@@ -1,104 +1,30 @@
-'use client'
-
-import { useQuery } from '@tanstack/react-query'
 import { Card } from 'primereact/card'
-import { Column } from 'primereact/column'
-import { DataTable } from 'primereact/datatable'
-import { Tag } from 'primereact/tag'
-import { getSubscriptions } from 'queries/subscriptions'
+import { TabView, TabPanel } from 'primereact/tabview'
 import { type ReactElement } from 'react'
-import { type Subscription } from 'utils/types'
-
-const totalRemaining = (subscriptions: Subscription[]): number => {
-  return subscriptions.reduce(
-    (acc: number, currentValue: Subscription) => acc + currentValue.remaining,
-    0
-  )
-}
-
-const totalPaid = (subscriptions: Subscription[]): number => {
-  return subscriptions.reduce(
-    (acc: number, currentValue: Subscription) =>
-      acc + (currentValue.total - currentValue.remaining),
-    0
-  )
-}
-
-const totalToPaid = (subscriptions: Subscription[]): number => {
-  return subscriptions.reduce(
-    (acc: number, currentValue: Subscription) => acc + currentValue.total,
-    0
-  )
-}
+import BillsTable from 'components/accounting/BillsTable'
+import { ConfirmDialog } from 'primereact/confirmdialog'
+import { InstructorPaymentsSection } from 'components/payments/InstructorPaymentSection'
+import { EmployeePaymentsSection } from 'components/payments/EmployeePaymentsSection'
 
 export default function Accounting(): ReactElement {
-  const { data: subscriptions } = useQuery({
-    queryKey: ['subscriptions'],
-    queryFn: async () => {
-      return await getSubscriptions()
-    }
-  })
-
   return (
     <Card className='h-full'>
-      <DataTable
-        header={() => (
-          <div className='flex align-items-center gap-4'>
-            <h2>Suscripciones</h2>
-            <Tag
-              value={`Total: ${totalToPaid(subscriptions ?? [])}`}
-              severity='info'
-            />
-            <Tag
-              value={`Pagado: ${totalPaid(subscriptions ?? [])}`}
-              severity='success'
-            />
-            <Tag
-              value={`Faltante: ${totalRemaining(subscriptions ?? [])}`}
-              severity='danger'
-            />
-          </div>
-        )}
-        value={subscriptions}
-        scrollable
-        scrollHeight='81dvh'
-        stripedRows
-      >
-        <Column
-          field='id'
-          header='ID'
-        />
-        <Column
-          field='member.name'
-          header='Alumno'
-        />
-        <Column
-          field='promotion.title'
-          header='Plan'
-        />
-        <Column
-          field='total'
-          header='Total'
-        />
-        <Column
-          field='remaining'
-          header='Faltante'
-        />
-        <Column
-          field='date'
-          header='Fecha'
-        />
-        <Column
-          field='paid'
-          header='Pagado'
-          body={(elem) => (
-            <Tag
-              value={elem.paid ? 'Si' : 'No'}
-              severity={elem.paid ? 'success' : 'danger'}
-            />
-          )}
-        />
-      </DataTable>
+      <ConfirmDialog />
+      <TabView>
+        <TabPanel header='Cobros'>
+          <BillsTable />
+        </TabPanel>
+        <TabPanel header='Pagos'>
+          <TabView>
+            <TabPanel header='Profesores'>
+              <InstructorPaymentsSection />
+            </TabPanel>
+            <TabPanel header='Empleados'>
+              <EmployeePaymentsSection />
+            </TabPanel>
+          </TabView>
+        </TabPanel>
+      </TabView>
     </Card>
   )
 }
