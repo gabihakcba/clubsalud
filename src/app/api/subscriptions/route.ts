bc43f54fa4954991ff473f393c35088ff7aa7f79
gabihakcba <gabrielhakcba@gmail.com>
2024-06-27 18:24:30 -0300

    change active and delete subscription

--- src/app/api/subscriptions/route.ts
+++ src/app/api/subscriptions/route.ts
@@ -1,4 +1,4 @@
-import { type Subscription, type PrismaClient } from '@prisma/client'
+import { type Subscription, type PrismaClient, Member } from '@prisma/client'
 import prisma from 'utils/prisma'
 import JSONbig from 'json-bigint'
 import { type NextRequest } from 'next/server'
@@ -18,6 +18,47 @@ export async function GET(): Promise<Response> {
     })
   } catch (error) {
     return new Response(JSONbig.stringify(error), {
+      status: 500
+    })
+  }
+}
+
+export async function PATCH(req: NextRequest): Promise<Response> {
+  try {
+    const data = await req.json()
+    const id = data.id as number
+
+    const subscription: Subscription | null = await db.subscription.findUnique({
+      where: { id }
+    })
+
+    /**
+     * If new subs is true, others must be false
+     */
+    if (!subscription?.active) {
+      const memberId = subscription?.memberId
+      const member: any = await db.member.findUnique({
+        where: { id: memberId },
+        include: { memberSubscription: true }
+      })
+      member?.memberSubscription.forEach(async (subs: Subscription) => {
+        await db.subscription.update({
+          where: { id: subs.id },
+          data: { active: false }
+        })
+      })
+    }
+
+    const updatedSubs = await db.subscription.update({
+      where: { id },
+      data: { active: !subscription?.active }
+    })
+
+    return new Response(JSON.stringify(updatedSubs), {
+      status: 200
+    })
+  } catch (error) {
+    return new Response(JSON.stringify('Server error'), {
       status: 500
     })
   }
@@ -63,6 +104,22 @@ export async function POST(req: NextRequest): Promise<Response> {
     console.log(error)
     return new Response(JSONbig.stringify(error), {
       status: 400
+    })
+  }
+}
+
+export async function DELETE(req: NextRequest): Promise<Response> {
+  try {
+    const { id } = await req.json()
+
+    const subscription = await db.subscription.delete({ where: { id } })
+
+    return new Response(JSON.stringify(subscription), {
+      status: 200
+    })
+  } catch (error) {
+    return new Response(JSON.stringify('Server error'), {
+      status: 500
     })
   }
 }
