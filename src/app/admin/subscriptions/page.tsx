653431f43833de490b3c16af0b45aec9d6dffa8b
gabihakcba <gabrielhakcba@gmail.com>
2024-06-30 02:16:26 -0300

    eslint hotfixes

--- src/app/admin/subscriptions/page.tsx
+++ src/app/admin/subscriptions/page.tsx
@@ -1,20 +1,12 @@
 'use client'
 
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import MemberSubsTable from 'components/subscriptions/MemberSubsTable'
-import SubscriptionTable from 'components/subscriptions/SubscriptionsTable'
-import { Button } from 'primereact/button'
 import { Card } from 'primereact/card'
-import { Column } from 'primereact/column'
-import { DataTable } from 'primereact/datatable'
-import { Tag } from 'primereact/tag'
 import { getMembers } from 'queries/members'
-import { deleteSubscription, updateSubscription } from 'queries/subscriptions'
-import { useState, type ReactElement } from 'react'
+import { type ReactElement } from 'react'
 
 export default function SubscriptionPage(): ReactElement {
-  const [memberSelected, setMemberSelected] = useState<any>(null)
-
   const { data: members } = useQuery({
     queryKey: ['members'],
     queryFn: async () => {
