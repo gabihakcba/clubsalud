882858e96d8d2adcfb8e267105f036f0aaa87de6
gabihakcba <gabrielhakcba@gmail.com>
2024-07-04 18:59:07 -0300

    changes on member attendance

--- src/app/admin/attendance/page.tsx
+++ src/app/admin/attendance/page.tsx
@@ -7,22 +7,6 @@ import { Permissions } from 'utils/types'
 import AttendanceAdmTable from 'components/attendance/AttendanceAdmTable'
 
 export default function Attendace(): ReactElement {
-  // const [user, setUser] = useState<Account | null>(null)
-
-  // const { data: classes } = useQuery({
-  //   queryKey: ['class'],
-  //   queryFn: async () => {
-  //     return await getClasses()
-  //   }
-  // })
-
-  // useEffect(() => {
-  //   const storage = localStorage.getItem('user')
-  //   if (storage) {
-  //     const user: Account = JSON.parse(storage)
-  //     setUser(user)
-  //   }
-  // }, [])
 
   return (
     <Card className='h-full w-full'>
