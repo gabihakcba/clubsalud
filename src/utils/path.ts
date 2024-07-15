d507bd564be5761dbbad2bdae06edc07c7507940
ClubSalud <clubsalud.medintt@gmail.com>
2024-07-15 20:19:02 -0300

    Update path.ts

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,6 +3,7 @@ export const test = 'https://clubsalud.vercel.app'
 export const production = 'https://clubsalud-eta.vercel.app/'
 
 export const path = (): string => {
+  console.log(process.env.NEXT_PUBLIC_NODE_ENV)
   if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
     return production
   } else if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
