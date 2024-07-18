d0500aa2330c3b612027bd35b37df2f7ac277c12
Gabriel Hak <gabrielhakcba@gmail.com>
2024-07-18 18:08:54 -0300

    new path function with env

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,11 +3,6 @@ export const production = 'https://clubsalud.vercel.app'
 export const test = 'https://stagingcs.vercel.app/'
 
 export const path = (): string => {
-  if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
-    return production
-  } else if (process.env.NEXT_PUBLIC_NODE_ENV === 'development') {
-    return development
-  } else {
-    return test
-  }
+  const path = process.env.NEXT_PUBLIC_NODE_PATH
+  return path ?? ''
 }
