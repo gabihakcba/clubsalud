f0704021b2531d08c37c2e64e2b7ca4e838601ff
gabriel <gabrielhakcba@gmail.com>
2024-08-29 19:18:58 -0300

    CRUD offers

---
+++ src/components/offers/CreateOfferForm.tsx
@@ -0,0 +1,90 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Button } from 'primereact/button'
+import { FloatLabel } from 'primereact/floatlabel'
+import { InputNumber } from 'primereact/inputnumber'
+import { InputText } from 'primereact/inputtext'
+import { createPlan } from 'queries/plan'
+import { useEffect, useState, type ReactElement } from 'react'
+import { useForm } from 'react-hook-form'
+import { Plan } from 'utils/types'
+
+export function CreateOfferForm(): ReactElement {
+  const { register, handleSubmit, setValue } = useForm()
+  const [durationMonth, setDurationMonth] = useState<number>(0)
+  const [discountPercent, setDiscountPercent] = useState<number>(0)
+
+  const query = useQueryClient()
+
+  const { mutate: create, isPending } = useMutation({
+    mutationFn: async (plan: Plan) => {
+      return await createPlan(plan)
+    },
+    onSuccess: async () => {
+      await query.refetchQueries({ queryKey: ['offers'] })
+    }
+  })
+
+  useEffect(() => {
+    setValue('durationMonth', 0)
+    setValue('discountPercent', 0)
+  }, [])
+
+  return (
+    <form
+      action=''
+      onSubmit={handleSubmit((data) => {
+        create(data as Plan)
+      })}
+      className='flex flex-column justify-content-start align-items-end gap-4 pt-4'
+    >
+      <FloatLabel>
+        <InputText {...register('title', { required: true })} />
+        <label htmlFor=''>Titulo</label>
+      </FloatLabel>
+
+      <FloatLabel>
+        <InputText {...register('description', { required: true })} />
+        <label htmlFor=''>Descripción</label>
+      </FloatLabel>
+
+      <FloatLabel>
+        <InputNumber
+          value={durationMonth}
+          {...register('durationMonth', { required: true, min: 1, max: 12 })}
+          min={0}
+          max={99999}
+          onChange={(e) => {
+            setValue('durationMonth', e.value)
+            setDurationMonth(Number(e.value))
+          }}
+        />
+        <label htmlFor=''>Duración en meses</label>
+      </FloatLabel>
+
+      <FloatLabel>
+        <InputNumber
+          value={discountPercent}
+          {...register('discountPercent', { required: true, min: 0, max: 60 })}
+          min={0}
+          max={99999}
+          minFractionDigits={0}
+          maxFractionDigits={3}
+          onChange={(e) => {
+            setValue('discountPercent', e.value)
+            setDiscountPercent(Number(e.value))
+          }}
+        />
+        <label htmlFor=''>Descuento total</label>
+      </FloatLabel>
+      <Button
+        type='submit'
+        label='Enviar'
+        size='small'
+        className='w-full'
+        icon='pi pi-upload'
+        iconPos='right'
+        loading={isPending}
+      />
+    </form>
+  )
+}
---
+++ src/components/offers/OffersTable.tsx
@@ -0,0 +1,149 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import HasRole from 'components/HasRole'
+import { Button } from 'primereact/button'
+import { Column } from 'primereact/column'
+import { confirmDialog } from 'primereact/confirmdialog'
+import { DataTable } from 'primereact/datatable'
+import { Dialog } from 'primereact/dialog'
+import { deletePlan, getPlan } from 'queries/plan'
+import { useState, type ReactElement } from 'react'
+import { Permissions, Plan } from 'utils/types'
+import { useModal } from 'utils/useModal'
+import { CreateOfferForm } from './CreateOfferForm'
+import { EditOfferForm } from './EditOfferForm'
+
+export default function OffersTable(): ReactElement {
+  const [selectedOffer, setSelectedOffer] = useState<Plan | null>(null)
+  const [createOffer, openCreateOffer, closeCreateOffer] = useModal(false)
+  const [editOffer, openEditOffer, closeEditOffer] = useModal(false)
+
+  const { data: offers, refetch } = useQuery({
+    queryKey: ['offers'],
+    queryFn: async () => {
+      const data = await getPlan()
+      return data
+    }
+  })
+
+  const { mutate: deleteOffer, isPending } = useMutation({
+    mutationFn: async (id: number) => {
+      return await deletePlan(id)
+    },
+    onSuccess: async () => {
+      refetch()
+    }
+  })
+
+  return (
+    <>
+      <Dialog
+        header='Crear Oferta'
+        visible={createOffer}
+        onHide={closeCreateOffer}
+      >
+        <CreateOfferForm/>
+      </Dialog>
+
+      <Dialog
+        header='Editar Oferta'
+        visible={editOffer}
+        onHide={closeEditOffer}
+      >
+        <EditOfferForm offer={selectedOffer}/>
+      </Dialog>
+
+      <DataTable
+        value={offers}
+        scrollable
+        scrollHeight='35dvh'
+        stripedRows
+        selectionMode='single'
+        selection={selectedOffer}
+        onSelectionChange={(e) => {
+          setSelectedOffer(e.value as Plan)
+        }}
+        header={() => (
+          <div className='flex gap-4 align-items-center'>
+            <h2>Ofertas</h2>
+            <HasRole required={[Permissions.ADM, Permissions.OWN]}>
+              <Button
+                label='Crear Oferta'
+                size='small'
+                icon='pi pi-plus'
+                iconPos='right'
+                onClick={openCreateOffer}
+              />
+            </HasRole>
+          </div>
+        )}
+      >
+        <Column
+          field='id'
+          header='ID'
+        />
+        <Column
+          field='title'
+          header='Nombre'
+        />
+          <Column
+            field='description'
+            header='Descripción'
+          />
+        <Column
+          field='durationMonth'
+          header='Meses'
+        />
+        <Column
+          field='discountPercent'
+          header='Descuento'
+        />
+        <Column
+          body={(offer) => (
+            <HasRole required={[Permissions.ADM, Permissions.OWN]}>
+              <Button
+                label='Editar'
+                size='small'
+                icon='pi pi-pen-to-square'
+                iconPos='right'
+                outlined
+                onClick={() => {
+                  setSelectedOffer(offer)
+                  openEditOffer()
+                }}
+              />
+            </HasRole>
+          )}
+        />
+        <Column
+          body={(offer) => (
+            <HasRole required={[Permissions.ADM, Permissions.OWN]}>
+              <Button
+                label='Eliminar'
+                size='small'
+                icon='pi pi-trash'
+                iconPos='right'
+                severity='danger'
+                outlined
+                loading={isPending && selectedOffer?.id === offer.id}
+                onClick={() => {
+                  setSelectedOffer(offer)
+                  confirmDialog({
+                    message: 'Confirmación de acción',
+                    header: 'Eliminar plan',
+                    icon: 'pi pi-info-circle',
+                    defaultFocus: 'reject',
+                    acceptClassName: 'p-button-danger',
+                    acceptLabel: 'Si',
+                    accept: () => {
+                      deleteOffer(Number(offer.id))
+                    }
+                  })
+                }}
+              />
+            </HasRole>
+          )}
+        />
+      </DataTable>
+    </>
+  )
+}
