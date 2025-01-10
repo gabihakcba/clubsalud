1ff308714f804e83af72708c9ca80a670d80430d
gabriel <gabrielhakcba@gmail.com>
2025-01-10 18:33:58 -0300

    no redirect medintt

--- src/app/(medintt)/page.tsx
+++ src/app/(medintt)/page.tsx
@@ -1,18 +1,13 @@
 'use client'
 
-import { useEffect, type ReactElement } from 'react'
+import { type ReactElement } from 'react'
 import CarouselMedintt from 'components/Medintt/Carousel'
 import Appointment from 'components/Medintt/Appointment'
 import Management from 'components/Medintt/Management'
 import Blog from 'components/Medintt/Blog'
 import Trust from 'components/Medintt/Trust'
-import { useRouter } from 'next/navigation'
 
 export default function Home(): ReactElement {
-  const router = useRouter()
-  useEffect(() => {
-    router.push('/clubsalud')
-  }, [])
   return (
     <div>
       <CarouselMedintt />
