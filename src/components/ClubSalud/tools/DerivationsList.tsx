30b19cc5f2723ef79427084e4fe28189e24006f9
gabriel <gabrielhakcba@gmail.com>
2025-01-20 16:22:07 -0300

    Derivations list

---
+++ src/components/ClubSalud/tools/DerivationsList.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { Column } from 'primereact/column'
+import { DataTable } from 'primereact/datatable'
+import { getMembers } from 'queries/ClubSalud/members'
+import { type ReactElement } from 'react'
+
+export default function DerivationList(): ReactElement {
+  const { data: members, isLoading } = useQuery({
+    queryKey: ['members'],
+    queryFn: async () => await getMembers()
+  })
+
+  return <DataTable value={members} loading={isLoading}>
+    <Column header='ID' field='id'/>
+    <Column header='Nombre' field='name'/>
+    <Column header='Apellido' field='lastName'/>
+    <Column header='Derivacion' field='derivatedBy'/>
+  </DataTable>
+}
